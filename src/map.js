849745718e2ab169f84504da0e36cb3f72945539
Erik Nordlund <me@nilserikjohan.se>
2019-06-11 19:10:40 +0200

    First stab at generating map with simplex-noise

--- src/map.js
+++ src/map.js
@@ -1,3 +1,4 @@
+import SimplexNoise from 'simplex-noise';
 import Voronoi from './voronoi';
 
 class Map {
@@ -7,18 +8,47 @@ class Map {
 
   static generate(numberOfCells, width, height) {
     const map = new Map();
+    map.width = width;
+    map.height = height;
+
     map.voronoi = Voronoi.generate(numberOfCells, width, height);
     map.voronoi.relax(3);
     map.cells = map.voronoi.cells();
+
+    map.generateNoise();
+
     return map;
+  }
+
+  generateNoise() {
+    this.noise = new SimplexNoise('seed');
   }
 
   render(canvas) {
     canvas.clear();
 
+    // for (let i = 0; i < 100; i += 1) {
+    //   for (let j = 0; j < 100; j += 1) {
+    //     const value = this.noise.noise2D(i, j);
+    //     const color = 255 * value;
+    //     const fillStyle = `rgb(${color}, ${color}, ${color}, 0.5)`;
+    //     console.log('FILLSTYLE', fillStyle);
+    //     canvas.drawCircle(i, j, 1, { fillStyle });
+    //   }
+    // }
+
     this.cells.forEach((cell) => {
+      const { x, y } = cell.path[0];
+      const value = this.noise.noise2D(x, y);
+      let fillStyle;
+      if (value >= 0.1) {
+        fillStyle = 'rgba(50, 200, 50, 0.7)';
+      } else {
+        fillStyle = 'rgba(10, 10, 250, 0.7)';
+      }
+
       canvas.drawPolygon(cell.path, {
-        fillStyle: cell.fillStyle,
+        fillStyle,
         strokeStyle: 'rgba(255, 0, 0, 0.5)',
       });
     });
