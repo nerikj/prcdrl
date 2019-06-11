a9966e72b54162b38573593f6667c3aecea85a5f
Erik Nordlund <me@nilserikjohan.se>
2019-06-11 18:35:19 +0200

    Please eslint in index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
-import Canvas from "./canvas";
-import Cell from "./cell";
-import Voronoi from "./voronoi";
-import Map from "./map";
+import Canvas from './canvas';
+import Map from './map';
 
 let canvas;
 let map;
-let voronoi;
 
 function addCursorDisplay() {
-  window.addEventListener("mousemove", function(event) {
-    const div = document.querySelector("#cursor");
-    const position = "X: " + event.clientX + " Y: " + event.clientY;
+  window.addEventListener('mousemove', (event) => {
+    const div = document.querySelector('#cursor');
+    const position = `X: ${event.clientX} Y: ${event.clientY}`;
     div.innerHTML = position;
   });
 }
 
 function drawDelaunay() {
-  for (let point of map.voronoi.points) {
-    canvas.drawCircle(point[0], point[1], 3, { fillStyle: "black" });
-  }
+  map.voronoi.points.forEach((point) => {
+    canvas.drawCircle(point[0], point[1], 3, { fillStyle: 'black' });
+  });
 
-  for (let polygon of map.voronoi.delaunayPolygons()) {
-    canvas.drawPolygon(polygon, { strokeStyle: "rgba(0, 0, 0, 0.2)" });
-  }
+  map.voronoi.delaunayPolygons().forEach((polygon) => {
+    canvas.drawPolygon(polygon, { strokeStyle: 'rgba(0, 0, 0, 0.2)' });
+  });
 
-  canvas.drawPolygon(map.voronoi.hullPolygon(), { strokeStyle: "rgba(0, 0, 0, 0.2)" });
+  canvas.drawPolygon(map.voronoi.hullPolygon(), { strokeStyle: 'rgba(0, 0, 0, 0.2)' });
+}
+
+function render() {
+  map.render(canvas);
+  drawDelaunay();
+}
+
+function resize() {
+  canvas.resize(window.innerWidth, window.innerHeight);
+  render();
 }
 
 function init() {
@@ -36,17 +43,7 @@ function init() {
   render();
 
   window.addEventListener('resize', resize, false);
-  // addCursorDisplay();
-}
-
-function render() {
-  map.render(canvas);
-  drawDelaunay();
-}
-
-function resize() {
-  canvas.resize(window.innerWidth, window.innerHeight);
-  render();
+  addCursorDisplay();
 }
 
 init();
