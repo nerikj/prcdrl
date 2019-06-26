bb24ee150ebc4ae049b5e12f6ae0891f31590f42
Erik Nordlund <me@nilserikjohan.se>
2019-06-26 18:54:37 +0200

    Rename main.js -> index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
